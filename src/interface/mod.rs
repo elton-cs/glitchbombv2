49c89f92ab171dc8fb1d6dbacb239649b5324dc3
elton-cs <govada.elton@gmail.com>
2025-08-06 10:37:37 -0500

    feat: single plugin good attempt v1

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,5 +1,3 @@
-use bevy::prelude::*;
-
 pub mod states;
 pub mod components;
 pub mod camera;
@@ -9,23 +7,3 @@ pub mod game_end;
 
 pub use states::*;
 pub use components::*;
-
-pub struct InterfacePlugin;
-
-impl Plugin for InterfacePlugin {
-    fn build(&self, app: &mut App) {
-        app.init_state::<GameState>()
-            .add_systems(Startup, camera::setup_camera)
-            .add_systems(OnEnter(GameState::Menu), menu::setup_menu_ui)
-            .add_systems(Update, menu::handle_start_button.run_if(in_state(GameState::Menu)))
-            .add_systems(OnExit(GameState::Menu), menu::cleanup_menu)
-            .add_systems(OnEnter(GameState::Playing), playing::setup_playing_ui)
-            .add_systems(Update, (playing::handle_quit_button, playing::handle_pull_orb_button).run_if(in_state(GameState::Playing)))
-            .add_systems(OnExit(GameState::Playing), playing::cleanup_playing)
-            .add_systems(OnEnter(GameState::GameWon), game_end::setup_game_won_ui)
-            .add_systems(Update, game_end::handle_restart_button.run_if(in_state(GameState::GameWon).or(in_state(GameState::GameLost))))
-            .add_systems(OnExit(GameState::GameWon), game_end::cleanup_game_end)
-            .add_systems(OnEnter(GameState::GameLost), game_end::setup_game_lost_ui)
-            .add_systems(OnExit(GameState::GameLost), game_end::cleanup_game_end);
-    }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@ use bevy::{asset::AssetMetaCheck, prelude::*};
 mod interface;
 mod game_state;
 
-use interface::InterfacePlugin;
-use game_state::GameStatePlugin;
+use interface::GameState;
 
 pub struct GamePlugin;
 impl Plugin for GamePlugin {
@@ -27,8 +26,82 @@ impl Plugin for GamePlugin {
                     .into(),
                     ..default()
                 }),
-        )
-        .add_plugins(InterfacePlugin)
-        .add_plugins(GameStatePlugin);
+        );
+        
+        app.init_state::<GameState>();
+        
+        app.add_systems(
+            Startup, 
+            interface::camera::setup_camera
+        );
+        
+        app.add_systems(
+            OnEnter(GameState::Menu), 
+            interface::menu::setup_menu_ui
+        );
+        
+        app.add_systems(
+            OnEnter(GameState::Playing), 
+            (
+                interface::playing::setup_playing_ui, 
+                game_state::systems::setup_game
+            ).chain()
+        );
+        
+        app.add_systems(
+            OnEnter(GameState::GameWon), 
+            interface::game_end::setup_game_won_ui
+        );
+        
+        app.add_systems(
+            OnEnter(GameState::GameLost), 
+            interface::game_end::setup_game_lost_ui
+        );
+        
+        app.add_systems(
+            Update, 
+            (
+                interface::menu::handle_start_button
+            ).run_if(in_state(GameState::Menu))
+        );
+        
+        app.add_systems(
+            Update, 
+            (
+                interface::playing::handle_quit_button, 
+                interface::playing::handle_pull_orb_button,
+                game_state::systems::update_stats_display, 
+                game_state::systems::check_win_loss_conditions
+            ).chain().run_if(in_state(GameState::Playing))
+        );
+        
+        app.add_systems(
+            Update, 
+            interface::game_end::handle_restart_button
+                .run_if(in_state(GameState::GameWon).or(in_state(GameState::GameLost)))
+        );
+        
+        app.add_systems(
+            OnExit(GameState::Menu), 
+            interface::menu::cleanup_menu
+        );
+        
+        app.add_systems(
+            OnExit(GameState::Playing), 
+            (
+                interface::playing::cleanup_playing, 
+                game_state::systems::cleanup_game
+            ).chain()
+        );
+        
+        app.add_systems(
+            OnExit(GameState::GameWon), 
+            interface::game_end::cleanup_game_end
+        );
+        
+        app.add_systems(
+            OnExit(GameState::GameLost), 
+            interface::game_end::cleanup_game_end
+        );
     }
 }
